1e6b7adb58e76583449db11cc3ba45926ce91e79
ERYKKA YUSTARI <erykka1800015027@webmail.uad.ac.id>
2022-05-27 15:58:31 +0700

    Update NeuralNetworks.py

--- NeuralNetworks.py
+++ NeuralNetworks.py
@@ -6,7 +6,12 @@ import pickle
 import copy
 
 nnfs.init()
+#Dense layer
+class Laye_Dense:
 
+    #Layer initialization
+    def__init__(self,n_inputs, n_neurons, weight_regularizer_L1=0,weight_regularizer_L2=0,bias_regularizer_L1=0,bias_regularizer_L2=0):
+    
 
 
 
