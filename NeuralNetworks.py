de4cb875f24770a8f6d2c660c3b1539806b548f9
widya agustina <80620726+Widya-ags@users.noreply.github.com>
2022-05-28 12:01:27 +0700

    Update NeuralNetworks.py

--- NeuralNetworks.py
+++ NeuralNetworks.py
@@ -168,7 +168,120 @@ class Layer_Dense:
             self.output = 1/(1+np.exp(-inputs))
 
         # Backward pass
+        def backward(self, dvaLues):
+            # Derivative - calculates from output of the sigmoid function
+            self.dinputs = dvalues * (1 - self.output)*self.output
         
+        # Calculate predictions for outputs
+        def predictions(self, outputs):
+            return (outputs > 0.5) * 1
+
+    # Linear activation
+    class Activation_Linear:
+
+        # Forwrad pass
+        def forward(self, inputs, training):
+            # Just remember values
+            self.inputs = inputs
+            self.outputs = inputs
+
+        # Backward pass
+        def backward(self, dvaLues):
+            # derivative is 1, 1 * dvaLues - the chain rule
+            self.dinputs = dvalues.copy()
+
+        # Calculate predictions for outputs
+        def predictions(self, outputs):
+            return outputs
+
+    # SGD optimizer 
+    class Optimizer_SGD:
+
+        # Initialize optimizer - set settings,
+        # learning rate of 1. is default for this optimizer
+        def __init__(self, Learning_rate =1., decay=0., momentum=0.):
+            self.learning_rate = learning_rate
+            self.current_learning_rate = learning_rate
+            self.decay = decay
+            self.iterations = 0
+            self.momentum = momentum
+
+        # Call once before any parameter updates
+        def pre_update_params(self):
+            if self.decay:
+                self.current_learning_rate = self.learning_rate * \ (1./(1.+ self.decay * self.iterations))
+
+        # update parameters
+        def update_params(Self, Layer):
+
+            # If we use momentum
+            if self.momentum:
+
+                # If layer does not contain momentum arrays, create them 
+                # filled with zeros
+                if not hasattr(layer, 'weight_momentums'):
+                    layer.weight_momentums = np.zeros_like(layer.weights)
+                    # If there is no momentum array for weight
+                    # The array doesn't exist for biases yet either.
+                    layer.bias_momentums = np.zeros_like(layer.biases)
+
+                # Build weight update with momentum -take previous
+                # Update multiplied by retain factor and update with 
+                # current gradients
+                weight_updates = \ self.momentum * layer.weight_momentums - \ self.current_learning_rate * layer.dweights
+                layer.weight_momentums = weight_updates
+
+                #Build bias updates
+                bias_updates = \ self.momentum * layer.bias_momentums - \ self.current_learning_rate * layer.dbiases
+                layer.bias_momentums = bias_updates
+
+                # Vanilla SGD updates (as before momentum update)
+                else :
+                    weight_updates = - self.current_learning_rate * \ layer.dweights
+                    bias_updates = - self.current_learning_rate * \ layer.dbiases
+
+                # Update weights and biases using either
+                # vanilla or momentum updates
+                layer.weights += weight_updates
+                layer.biases += bias_updates
+
+            # Call once after any parameter updates
+            def post_update_params(self):
+                self.iterations += 1
+        
+    # Adagrad optimizer
+    class Optimizer_Adagrad:
+
+        # Initialize optimizer - set settings
+        def __init__(self, Learning_rate=1., decay=0., epsilon=1e-7):
+            self.learning_rate = Learning_rate
+            self.current_learning_rate = Learning_rate 
+            self.decay = decay
+            self.iterations = 0
+            self.epsilon = epsilon
+        
+        # Call once before any parameter updates
+        def pre_update_params(self):
+            if self.decay:
+                self.current_learning_rate = self.learning_rate*\(1./(1. + self.decay * self.iterations))
+        
+        # Update parameters
+        def update_params(self, Layer):
+
+            # If layer does not contain cache arrays,
+            # Create them filled with zeros
+            if not hasattr(layer, 'weight_cache'):
+                layer.weight_cache = np.zeros_like(layer.weights)
+                layer.bias_cache = np.zeros_like(layer.biases)
+        
+        # Update cache with squared current gradients
+        layer.weight_cache += layer.dweights**2
+        layer.bias_cache += layer.dbiases**2
+
+
+
+
+
 
 
         
