025d1e77e387839518d670c254b54162a3bacc70
Indra Cahya Ramdani <37335233+indracahyaramdani@users.noreply.github.com>
2022-05-28 12:36:07 +0700

    Update NeuralNetworks.py

--- NeuralNetworks.py
+++ NeuralNetworks.py
@@ -1,3 +1,5 @@
+from random import sample
+from turtle import forward
 import numpy as np
 import nnfs
 import os
@@ -13,6 +15,335 @@ class Laye_Dense:
     def __init__(self,n_inputs, n_neurons, weight_regularizer_L1=0,weight_regularizer_L2=0,bias_regularizer_L1=0,bias_regularizer_L2=0):
 
     
+
+# Cross-entropy loss
+class Loss_CtagoricalCrossentropy(Loss):
+
+    # Forward pass
+    def forward(self, y_pred, y_true):
+
+        # Number of samples in a batch
+        samples = len(y_pred)
+
+        #Clip data to prevent division by 0
+        #Clip both sides to not drag mean towards any value
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
+
+        #Probabilities for target values -
+        #Only if categorical labels
+        if len(y_true.shape) == 1:
+            correct_confidences = y_pred_clipped[
+                range(samples),
+                y_true
+            ]
+
+        #Mask values - only for one-hot encoded labels
+        elif len(y_true.shape) ==2:
+            correct_confidences = np.sum(
+                y_pred_clipped * y_true,
+                axis=1
+            )
+
+        #Losses
+        negative_log_likelihoods = -np.log(correct_confidences)
+        return negative_log_likelihoods
+
+    # Backward pass
+    def backward(self, dvalues, y_true):
+
+        # Number o samples
+        samples = len(dvalues)
+        # Number of labels in every sample
+        # We'll use the first sample to count them
+        labels = len(dvalues[0])
+
+        # If labels are sparse, turn them into one-hot vector
+        if len(y_true.shape) == 1:
+            y_true = np.eye(labels)[y_true]
+
+        # Calculate gradient
+        self.dinputs = -y_true / dvalues
+        # Normalize gradient
+        self.dinputs = self.dinputs / samples
+
+# Softmax classifier - combined softmax activation
+# And cross-entropy loss for faster backward step
+class Activation_Softmax_Loss_CategoricalCrossentropy():
+
+    # Backward pass
+    def backward(self, dvalues, y_true):
+
+        # Number of samples
+        samples = len(dvalues)
+
+        # If labels are one-hot encoded,
+        # Turn them into discrete values
+        if len(y_true.shape) == 2:
+            y_true = np.argmax(y_true, axis=1)
+
+        # Copy so we can safely modify
+        self.dinputs = dvalues.copy()
+        # Calculate gradient
+        self.dinputs[range(samples), y_true] -=1
+        # normalize gradient
+        self.dinputs = self.dinputs / samples
+
+
+# Binary cross-entropy loss
+class Loss_BinaryCrossentropy(Loss):
+
+    # Forward pass
+    def forward(self, y_pred, y_true):
+
+        # Clip data to prevent division by 0
+        # Clip both sides to not drag mean towards any value
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
+
+        # Calculate sample-wise loss
+        sample_losses = -(y_true * np.log(y_pred_clipped) +
+        (1 - y_true) * np.log(1 - y_pred_clipped))
+        sample_losses = np.mean(sample_losses, axis=-1)
+
+        # Return losses
+        return sample_losses
+
+    # Backward pass
+    def backward(self, dvalues, y_true):
+
+        # Number of samples
+        samples = len(dvalues)
+        # Number of outputs in every sample
+        # We'll use the first sample to count them
+        outputs = len(dvalues[0])
+
+        # Clip data to prevent division by 0
+        # Clip both sides to not drag mean towards any value
+        clipped_dvalues = np.clip(dvalues, 1e-7, 1 - 1e-7)
+
+        # Calculate gradient
+        self.dinputs = -(y_true / clipped_dvalues - 
+        (1 - y_true) / (1 - clipped_dvalues)) / outputs
+        # Normalize gradient
+        self.dinputs = self.dinputs / samples
+
+# Mean square error loss
+class Loss_MeanSquaredError(Loss): #L2 loss
+
+    # Forward pass
+    def forward(self, y_pred, y_true):
+
+        # Calculate loss
+        sample_losses = np.mean((y_true - y_pred)**2, axis=-1)
+
+        # Return losses
+        return sample_losses
+
+    # Backward pass
+    def backward(self, dvalues, y_true):
+
+        # Number of sample
+        samples = len(dvalues)
+        # Number of outputs in every sample
+        # We'll use the first sample to count them
+        outputs = len(dvalues[0])
+
+        # Gradient on values
+        self.dinputs = -2 * (y_true - dvalues) / outputs
+        # Normalize gradient
+        self.dinputs = self.dinputs / samples
+
+# Mean Absolute Error loss
+class Loss_MeanAbsoluteError(Loss): #L1 loss
+
+    def forward(self, y_pred, y_true):
+
+        # Calculate loss
+        sample_losses = np.mean(np.abs(y_true - y_pred), axis=-1)
+
+        # Return losses
+        return sample_losses
+
+    # Backward pass
+    def backward(self, dvalues, y_true):
+
+        # Number of samples
+        samples = len(dvalues)
+        # Number of outputs in every sample
+        # We'll use the first sampe to count them
+        outputs = len(dvalues[0])
+
+        # Calculate gradient
+        self.dinputs = np.sign(y_true - dvalues) / outputs
+        # Normalize gradient
+        self.dinputs = self.dinputs / samples
+
+# Common accuracy class
+class Accuracy:
+
+    # Calculate an accuracy
+    # given prediction and griund truth values
+    def calculate(self, predictions, y):
+
+        # Get comparison results
+        comparisons = self.compare(predictions, y)
+
+        # Calculate accuracy
+        Accuracy = np.mean(comparisons)
+
+        # Add accumulated sum of matching values and sample count
+        self.accumulated_sum += np.sum(comparisons)
+        self.accumulated_count += len(comparisons)
+
+        # Return accuracy
+        return Accuracy
+
+    # Calculates accumulated accuracy
+    def calculate_accumulated(self):
+
+        # Calculate an accuracy
+        Accuracy = self.accumulated_sum / self.accumulated_count
+
+        # Return the data and regularization losses
+        return Accuracy
+
+    # Reset variabels for accumulated accuracy
+    def new_pass(self):
+        self.accumulated_sum = 0
+        self.accumulated_count = 0
+
+# Accuracy calculation for classification model
+class Accuracy_Categorical(Accuracy):
+
+    # No initialization is needed
+    def init(self, y):
+        pass
+
+    # Compares predictions to the ground truth values
+    def compare(self, predictions, y):
+        if len(y.shape) == 2:
+            y = np.argmax(y, axis=1)
+        return predictions == y
+
+# Accuracy calculation for regression model
+class Accuracy_Regression(Accuracy):
+
+    def __init__(self):
+        # Create precision property
+        self.precision = None
+
+    # Calculate precision value
+    # Based on passed in ground truth value
+    def init(self, y, reinit=False):
+        if self.precision is None or reinit:
+            self.precision = np.std(y) / 250
+
+    # Compares predictions to the ground truth values
+    def compare(self, predictions, y):
+        return np.absolute(predictions - y) < self.precision
+
+# Model class
+class Model:
+
+    def __init__(self):
+        # Create a list of network objects
+        self.layers = []
+        # Softmax classifier's object
+        self.softmax_classifier_output = None
+
+    # Add object to the model
+    def add(self, Layer):
+        self.layers.append(Layer)
+
+    # Set loss, optimizer and accuracy
+    def set(self, *, Loss=None, optimizer=None, accuracy=None):
+
+        if loss is not None:
+            self.loss = Loss
+
+        if optimizer is not None:
+            self.optimizer = optimizer
+
+        if accuracy is not None:
+            self.accuracy = accuracy
+
+    # Finalize the model
+    def finalize(self):
+
+        # Create and set the input layer
+        self.input_layer = Layer_Input()
+
+        # Count all the objects
+        layer_count = len(self.layers)
+
+        # Initialize a list containing trainable layers:
+        self.trainable_layers = []
+
+        # Iterate the objects
+        for i in range(layer_count):
+
+            # If it's the first layer,
+            # the previous layer object is the input layer
+            if i == 0:
+                self.layers[i].prev = self.input_layer
+                self.layers[i].next = self.layers[i+1]
+
+            # All layers except for the first and the last
+            elif i < layer_count - 1:
+                self.layers[i].prev = self.layers[i-1]
+                self.layers[i].next = self.layers[i+1]
+
+            # the last layer - the next objects is the loss
+            # Also let's save aside the reference to the last object
+            # whose output is the model's output
+            else:
+                self.layers[i].prev = self.layers[i-1]
+                self.layers[i].next = self.loss
+                self.output_layer_activation = self.layers[i]
+
+            #if layer contains an atribute called "wight",
+            #it's a trainable layer -
+            #add it to the list of trainable layers
+            #we dont need to check for biases
+            #checking for wights is enough
+            if hasattr(self.layers[i], 'weights'):
+                self.trainable_layers.append(self.layers[i])
+
+            # Update loss object with trainable layers
+            if self.loss is not None:
+                self.loss.remember_trainable_layers(
+                    self.trainable_layers
+                )
+
+        # if output activation is not softmax and
+        # loss function is categorical cross-entropy
+        # create an object of combined activation
+        # and loss function containing
+        # faster gradient calculation
+        if isinstance(self.layers[-1], Activation_Softmax) and \ isinstance(self.loss, Loss_CtagoricalCrossentropy):
+
+#Train the model
+def train(self, X, y, *, epochs=1, batch_size=None, print_everu=1, validation_data=None):
+
+    # Initialize accuracy object
+    self.accuracy.init(y)
+
+    # Default value if batch size is not being set
+    train_steps = 1
+
+    # Calculate number of steps
+    if batch_size is not None:
+        train_steps = len(X) // batch_size
+        # Dividing rounds down. If there are some reamining
+        # data but not a full batch, this won't include it
+        # Add '1' to include this not full batch
+        if train_steps * batch_size < len(X):
+            train_steps += 1
+            
+
+
+
+    
+
 
 
 
